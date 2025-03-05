06b192e741b5cb4a590f4672ab7aa468f322edf3
Mauricio <mauricio@gmail.com>
2025-03-04 22:31:31 -0500

    creation of the archetype

---
+++ src/test/java/com/powerup/house_microservice/HouseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerup.house_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
